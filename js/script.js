06a1cb2286e537d8e414725fd98057a8273773ca
Amelie121212 <amel.1989ab@gmail.com>
2020-05-14 23:59:25 +0100

    carousel

---
+++ js/script.js
@@ -0,0 +1,66 @@
+// Scroll to top
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    var top = document.getElementById('totop');
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        top.style.display = 'block';
+    } else {
+        top.style.display = 'none';
+    }
+};
+function scrollToTop() {
+    var position = document.body.scrollTop || document.documentElement.scrollTop;
+    if (position) {
+        window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
+        scrollAnimation = setTimeout('scrollToTop()', 30);
+    } else clearTimeout(scrollAnimation);
+};
+ // Message send
+var send = document.querySelector('#send');
+if (send) {
+     send.addEventListener('click', ()=> {
+        event.preventDefault();
+        setTimeout(() => {
+            document.getElementById('message').classList.remove('hide');                
+        }, 500); 
+    });
+}
+
+// ----carousel
+var carousel = document.querySelector('.carousel');
+var cells = carousel.querySelectorAll('.carousel_cell');
+var cellCount = 6;
+var selectedIndex = 0;
+var cellWidth = carousel.offsetWidth;
+var rotateFn = 'rotateY';
+var radius, theta;
+theta = 360 / cellCount;
+radius = Math.round( ( cellWidth / 2) / Math.tan( Math.PI / cellCount ) );
+
+// set position
+for ( var i=0; i < 6; i++ ) {
+    var cell = cells[i];
+    if ( i < cellCount ) {
+      cell.style.opacity = 1;
+      var cellAngle = theta * i;
+      cell.style.transform = rotateFn + '(' + cellAngle + 'deg) translateZ(' + radius + 'px)';
+    } 
+}
+
+function rotateCarousel() {
+  var angle = theta * selectedIndex * -1;
+  carousel.style.transform = 'translateZ(' + -radius + 'px) ' + 
+    rotateFn + '(' + angle + 'deg)';
+}
+
+var prevButton = document.querySelector('.previous-button');
+prevButton.addEventListener( 'click', function() {
+  selectedIndex--;
+  rotateCarousel();
+});
+
+var nextButton = document.querySelector('.next-button');
+nextButton.addEventListener( 'click', function() {
+  selectedIndex++;
+  rotateCarousel();
+});
